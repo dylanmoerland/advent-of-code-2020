6a1f665cb17c920610c05a3cecfaa3a5747a8e5c
Dylan Moerland <dylan@headfwd.com>
2020-12-10 18:29:03 +0200

    fix: day10 use correct default value for accumulator

--- src/day10/part2.ts
+++ src/day10/part2.ts
@@ -4,20 +4,23 @@ const run = async () => {
   const items = await readText("src/day10/data.txt");
 
   const numbers = items
-    .map((item: string) => parseInt(item, 10))
+    .map((item) => parseInt(item, 10))
     .sort((a, b) => b - a);
 
-  const options = numbers.reduce((acc, crr) => {
-    if (crr === Math.max(...numbers)) return { ...acc, [Math.max(...numbers)]: 1 };
+  const options = numbers.reduce(
+    (acc, crr) => {
+      if (crr === Math.max(...numbers)) return acc;
 
-    return {
-      ...acc,
-      [crr]: [crr + 1, crr + 2, crr + 3].reduce((a, c) => {
-        if (acc[c]) return a + acc[c];
-        return a;
-      }, 0),
-    };
-  }, {} as Record<number, number>);
+      return {
+        ...acc,
+        [crr]: [crr + 1, crr + 2, crr + 3].reduce((a, c) => {
+          if (acc[c]) return a + acc[c];
+          return a;
+        }, 0),
+      };
+    },
+    { [Math.max(...numbers)]: 1 }
+  );
 
   return options[1] + options[2] + options[3];
 };
